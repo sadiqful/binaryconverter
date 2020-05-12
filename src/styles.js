e76dc92fb5a17b887ff6a75caf086bf0c6530716
Aliyu Abubakar <sadiqful@gmail.com>
2020-05-12 10:39:06 +0100

    Styled form
    styles for styled form tag

---
+++ src/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form `
+border: 1px solid gray;
+border-radius: 5px;
+padding: 10px;
+width: 5px;
+background: radial-gradient(ellipse at center, #f2f9fe 0%, #d6f0fd 100%);
+`
