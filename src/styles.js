8021a5228a1d59be2ff9fcc00a7da9be89b8d89b
Aliyu Abubakar <sadiqful@gmail.com>
2020-05-12 10:41:05 +0100

    field style
    style for field tag

--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const StyledForm = styled.form `
-border: 1px solid gray;
-border-radius: 5px;
-padding: 10px;
-width: 5px;
-background: radial-gradient(ellipse at center, #f2f9fe 0%, #d6f0fd 100%);
-`
+export const StyledForm = styled.form`
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 10px;
+  width: 5px;
+  background: radial-gradient(ellipse at center, #f2f9fe 0%, #d6f0fd 100%);
+`;
+
+export const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
